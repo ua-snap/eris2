31e8d194b69c94f989e670109ac8a15d64ed4291
Matt Leonawicz <matt.rstat@gmail.com>
2015-02-04 13:52:56 -0700

    Clean up

--- plot3D/ui.R
+++ plot3D/ui.R
@@ -14,7 +14,7 @@ shinyUI(fluidPage(theme="cyborg_bootstrap.css",
 	),
 	headerPanel_2(
 		HTML('Examples using plot3D and rgl with Shiny
-			<a href="http://snap.uaf.edu" target="_blank"><img id="stats_logo" align="right" alt="SNAP Logo" src="./img/SNAP_acronym_for-dark-bkgrnd_100px.png" />/a>'
+			<a href="http://snap.uaf.edu" target="_blank"><img id="stats_logo" align="right" alt="SNAP Logo" src="./img/SNAP_acronym_for-dark-bkgrnd_100px.png" /></a>'
 		), h4, "plot3D and rgl in Shiny"
 	),
 	fluidRow(
